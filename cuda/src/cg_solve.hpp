c9f65f9a630673d87d787e94152801b11fb18642
Lukas Spies <LSpies@illinois.edu>
2022-02-16 17:09:27 -0600

    add tausch to cuda (non overlapping)

--- cuda/src/cg_solve.hpp
+++ cuda/src/cg_solve.hpp
@@ -86,7 +86,8 @@ cg_solve(OperatorType& A,
          typename TypeTraits<typename OperatorType::ScalarType>::magnitude_type& tolerance,
          typename OperatorType::LocalOrdinalType& num_iters,
          typename TypeTraits<typename OperatorType::ScalarType>::magnitude_type& normr,
-         timer_type* my_cg_times)
+         timer_type* my_cg_times,
+         Tausch *tausch)
 {
   typedef typename OperatorType::ScalarType ScalarType;
   typedef typename OperatorType::GlobalOrdinalType GlobalOrdinalType;
@@ -141,7 +142,7 @@ cg_solve(OperatorType& A,
   TICK(); waxpby(one, x, zero, x, p); TOCK(tWAXPY);
 
   TICK();
-  matvec(A, p, Ap);
+  matvec(A, p, Ap, tausch);
   TOCK(tMATVEC);
 
   TICK(); waxpby(one, b, -one, Ap, r); TOCK(tWAXPY);
@@ -181,7 +182,7 @@ cg_solve(OperatorType& A,
     magnitude_type alpha = 0;
     magnitude_type p_ap_dot = 0;
 
-    TICK(); matvec(A, p, Ap); TOCK(tMATVEC);
+    TICK(); matvec(A, p, Ap, tausch); TOCK(tMATVEC);
 
     TICK(); p_ap_dot = dot(Ap, p); TOCK(tDOT);
 
